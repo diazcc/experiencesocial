474cb07bad8ca7ac9687ad22ca12c23f5f17c6e6
Andres Diaz <diazccx@gmail.com>
2026-03-15 12:39:27 -0500

    :art: update title in index.html and simplify Login.template.vue

--- src/components/templates/login/Login.template.vue
+++ src/components/templates/login/Login.template.vue
@@ -2,34 +2,11 @@
     <main class="login__wrapper">
         <header class="login__wrapper__header">
             <h1 style="color:white;font-size: 24px;font-weight: lighter;">
-                ALMOTORES - Andres Diaz Castillo - diazccx@gmail.com - 3146724568   </h1>
-            <SelectLangMolecule/>
+             Andres Diaz Castillo   </h1>
         </header>
         <article
             :class="'login__wrapper__content login__wrapper__content--' + (!dataLogin.stateForm ? 'register' : '')">
-            <section class="login__wrapper__content__header">
-                <img src="/door-icon.png" alt="door-icon">
-                <Title :dataTitle="dataLogin.dataTitle" />
-            </section>
-            <ul
-                :class="'login__wrapper__content__options login__wrapper__content__options--' + (!dataLogin.stateForm ? 'register' : '')">
-                <CardOptionLogin :dataCardOptionLogin="dataLogin.dataCardOptionLogin"
-                    class="login__wrapper__content__options__card" />   
-            </ul>
-            <section
-                :class="'login__wrapper__content__form login__wrapper__content__form--' + (!dataLogin.stateForm ? 'register' : '')+' login__wrapper__content__form login__wrapper__content__form--' + (!dataLogin.visible ? 'hidde' : '')">
-                <InputText :dataInput="dataLogin.dataInputName"
-                    v-if="!dataLogin.stateForm " required />
-                <InputText :dataInput="dataLogin.dataInputEmail" required />
-                <InputText :dataInput="dataLogin.dataInputPassword" required />
-                <Button :dataButton="dataLogin.dataButton" />
-            </section>
-            <!-- <p class="login__wrapper__content__forgot" v-if="dataLogin.stateForm" @click="dataLogin.sendVerificationCode">
-                {{ t('have_you_forgotten_your_password') }}
-            </p>
-            <p class="login__wrapper__content__forgot" v-if="dataLogin.stateLogin=='user'" @click="dataLogin.redirectFiling">
-                {{ t('continue_without_user') }}
-            </p> -->
+           
         </article>
         <!-- <Footer class="login__wrapper__footer" :imgColor="'white'"/> -->
 
@@ -48,7 +25,7 @@ import Title from '../../atoms/title/Title.atom.vue';
 import Text from '../../atoms/text/Text.atom.vue';
 import OptionsLogin from '../../organisms/options-login/OptionsLogin.organism.vue';
 import CardLoginNormal from '../../organisms/card-login-normal/CardLoginNormal.organism.vue';
-import { defineProps } from 'vue';
+
 import InputText from '../../atoms/input/InputText.atom.vue';
 import Button from '../../atoms/button/Button.atom.vue';
 import { useI18n } from 'vue-i18n';
