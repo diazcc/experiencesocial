81e5b489cbb190af027c8c5bbb54040bc4ac3484
Andres Diaz <diazccx@gmail.com>
2026-03-15 13:11:57 -0500

    :art: add BackgroundBubbles component and integrate into Login template

---
+++ src/components/atoms/background-bubbles/BackgroundBubbles.atom.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="lava-background">
+        <!-- SVG Filter (Oculto) que crea el efecto de líquido/aceite -->
+         l
+        <svg class="lava-background__filter" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <filter id="gooey-effect">
+                    <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
+                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -10" result="gooey-effect" />
+                    <feBlend in="SourceGraphic" in2="gooey-effect" />
+                </filter>
+            </defs>
+        </svg>
+
+        <!-- Contenedor al que se le aplica el filtro -->
+        <div class="lava-background__blobs-container">
+            <div class="lava-background__blob lava-background__blob--1"></div>
+            <div class="lava-background__blob lava-background__blob--2"></div>
+            <div class="lava-background__blob lava-background__blob--3"></div>
+            <div class="lava-background__blob lava-background__blob--4"></div>
+            <div class="lava-background__blob lava-background__blob--5"></div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// Este componente es puramente visual, no requiere lógica compleja.
+// Se usa como fondo absoluto dentro de un contenedor relativo.
+</script>
+
+<style scoped src="./BackgroundBubbles.atom.scss" lang="scss">
+  
+</style>
--- src/components/templates/login/Login.template.vue
+++ src/components/templates/login/Login.template.vue
@@ -5,31 +5,16 @@
              Andres Diaz Castillo   </h1>
         </header>
         <article
-            :class="'login__wrapper__content login__wrapper__content--' + (!dataLogin.stateForm ? 'register' : '')">
-           
+            class='login__wrapper__content '>hhh
+           <BackgroundBubblesAtom/>
         </article>
-        <!-- <Footer class="login__wrapper__footer" :imgColor="'white'"/> -->
-
-        <ModalAlert :dataModalAlert="dataLogin.dataModalAlert" />
-        <ModalConfirmation :dataModalConfirmation="dataLogin.dataModalConfirmation" />
     </main>
 </template>
 
 <script setup lang="ts">
-import SelectLangMolecule from '../../molecules/select-lang/SelectLang.molecule.vue';
-import CardOptionLogin from '../../molecules/card-option-login/CardOptionLogin.molecule.vue'
-import ModalAlert from '../../molecules/modals/alert/ModalAlert.molecule.vue';
-import ModalConfirmation from '../../molecules/modals/confirmation/ModalConfirmation.molecule.vue';
-import FormLogin from '../../organisms/form-login/FormLogin.organism.vue';
-import Title from '../../atoms/title/Title.atom.vue';
-import Text from '../../atoms/text/Text.atom.vue';
-import OptionsLogin from '../../organisms/options-login/OptionsLogin.organism.vue';
-import CardLoginNormal from '../../organisms/card-login-normal/CardLoginNormal.organism.vue';
 
-import InputText from '../../atoms/input/InputText.atom.vue';
-import Button from '../../atoms/button/Button.atom.vue';
 import { useI18n } from 'vue-i18n';
-import Footer from '../../organisms/footer/Footer.organism.vue';
+import BackgroundBubblesAtom from '../../atoms/background-bubbles/BackgroundBubbles.atom.vue';
 
 const { t,locale } = useI18n();
 defineProps(['dataLogin']);//Use defineProps for can get data in another component.
